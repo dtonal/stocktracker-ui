877af50200cc00f52b1efe1dfd4e5a9d025c9e5a
dtonal <dtonal@posteo.de>
2025-07-15 11:54:43 +0200

    The application has been simplified by removing unused components (HelloWorld, TheWelcome) and associated assets (icons). App.vue and HomeView.vue were updated to streamline navigation and display a welcome message. The CSS was adjusted for an optimized layout.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,14 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Willkommen beim Stocktracker!</h1>
   </main>
 </template>
+
+<style scoped>
+main {
+  width: 100%;
+  text-align: center;
+}
+</style>
