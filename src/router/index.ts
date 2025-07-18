9f757ab50a693a5ac9f888d6fc68895023073d88
dtonal <dtonal@posteo.de>
2025-07-18 16:35:15 +0200

    fixed error by removing not existing about reference

--- src/router/index.ts
+++ src/router/index.ts
@@ -21,15 +21,6 @@ const router = createRouter({
       name: 'register',
       component: () => import('../views/RegisterView.vue'),
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-      meta: { requiresAuth: true },
-    },
   ],
 })
 
